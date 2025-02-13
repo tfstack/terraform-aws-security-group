a5bc14ca005c7d2283985db1c57dcb38a79056c1
John Ajera <37360952+jajera@users.noreply.github.com>
2025-02-13 07:12:30 +0000

    feat: add msk port 9098
    adds msk port

--- variables.tf
+++ variables.tf
@@ -18,6 +18,7 @@ locals {
     "jenkins-8080-tcp"       = [8080, 8080, "tcp", "Jenkins CI/CD Server"]
     "kafka-9092-tcp"         = [9092, 9092, "tcp", "Apache Kafka"]
     "kafka-9094-tcp"         = [9094, 9094, "tcp", "Apache Kafka Broker TLS"]
+    "kafka-9098-tcp"         = [9098, 9098, "tcp", "Apache Kafka IAM Authentication"]
     "ldap-389-tcp"           = [389, 389, "tcp", "LDAP Directory"]
     "ldaps-636-tcp"          = [636, 636, "tcp", "Secure LDAP"]
     "memcached-11211-tcp"    = [11211, 11211, "tcp", "Memcached Cache"]
